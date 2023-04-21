f03e6fbbbd0958e3c65f16ee53b2fdbb2b4aacc7
Aman07a <aman16.aa17@gmail.com>
2023-04-21 11:38:18 +0200

    Installing Android Studio

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello World! + Installing Android Studio</Text>
       <StatusBar style="auto" />
     </View>
   );
