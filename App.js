86495c792024b60c80782e6b1a900638d00a998c
Aman07a <aman16.aa17@gmail.com>
2023-04-24 09:12:44 +0200

    Starting Setup

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>Finished Sections:</Text>
-      <Text>Section 2: React Native Basics</Text>
+      <Text>Hello World!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
