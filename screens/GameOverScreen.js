87d1093b28633118df93e9bd67716ccf1600552b
Aman07a <aman16.aa17@gmail.com>
2023-04-25 08:43:15 +0200

    Improving the Game Screen Visuals

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,14 @@
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return <Text style={styles.screen}>Game is over!</Text>;
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
