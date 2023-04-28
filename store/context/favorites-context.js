91d23e9207f1e2d2dd9784079870f7462a3dc802
Aman07a <aman16.aa17@gmail.com>
2023-04-28 11:49:35 +0200

    Getting Started with React's Context API

---
+++ store/context/favorites-context.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const FavoritesContext = createContext({
+  id: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
+});
+
+function FavoritesContextProvider({ children }) {
+  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
+}
+
+export default FavoritesContextProvider;
