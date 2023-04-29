27c01fb8754ecab3ea2b3b848271b46f83ebc10d
Aman07a <aman16.aa17@gmail.com>
2023-04-29 10:27:25 +0200

    Outputting a List of Expenses

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,7 @@
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 Days"/>;
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
 }
 
 export default RecentExpenses;
