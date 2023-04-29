23d3d3c45772847c814536ebbc94ec230d3f8104
Aman07a <aman16.aa17@gmail.com>
2023-04-29 09:44:15 +0200

    Creating Key App Components to Display Expenses

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,13 @@
+function ExpensesSummary() {
+  return (
+    <View>
+      <View>
+        <Text>Last 7 days</Text>
+        <Text>$177.95</Text>
+      </View>
+      <FlatList />
+    </View>
+  );
+}
+
+export default ExpensesSummary;
