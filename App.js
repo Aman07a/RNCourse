976a0fe55d669d1a11a3f8afac31a23dd39ec237
Aman07a <aman16.aa17@gmail.com>
2023-05-02 18:32:11 +0200

    Starting Project

--- App.js
+++ App.js
@@ -1,63 +1,57 @@
-import React, { useState } from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
-import Block from "./components/Block";
-import Title from "./components/Title";
+import { Colors } from "./constants/styles";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
   return (
-    <View style={styles.container}>
-      <View style={styles.blockContainer}>
-        <View style={styles.titleContainer}>
-          <Title>Finished Sections</Title>
-        </View>
-        <View style={styles.row}>
-          <Block>Section 1: Getting Started</Block>
-          <Block>Section 2: React Native Basics</Block>
-        </View>
-        <View style={styles.row}>
-          <Block>Section 3: Debugging React Native Apps</Block>
-          <Block>
-            Section 4: Diving Deeper into Components, Layouts & Styling
-          </Block>
-        </View>
-        <View style={styles.row}>
-          <Block>
-            Section 5: Building Adaptive User Interfaces (Adapt to Platform &
-            Device Sizes)
-          </Block>
-        </View>
-      </View>
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
   );
-};
+}
 
-export default App;
+function AuthenticatedStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    </Stack.Navigator>
+  );
+}
 
-const deviceWidth = Dimensions.get("window").width;
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginHorizontal: 20,
-  },
-  blockContainer: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    marginTop: 20,
-  },
-  titleContainer: {
-    marginBottom: 10,
-    padding: deviceWidth < 380 ? 12 : 12,
-    margin: deviceWidth < 380 ? 12 : 24,
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+
+      <Navigation />
+    </>
+  );
+}
