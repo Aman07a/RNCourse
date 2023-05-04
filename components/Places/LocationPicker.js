9fa902f539fa44634ed0f78565335f6a4c8c23bf
Aman07a <aman16.aa17@gmail.com>
2023-05-04 12:24:08 +0200

    Removed Google Maps

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -3,16 +3,12 @@ import {
   getCurrentPositionAsync,
   useForegroundPermissions,
 } from "expo-location";
-import { useState } from "react";
-import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 
 import { Colors } from "../../constants/colors";
-import { getMapPreview } from "../../util/location";
 import OutlinedButton from "../UI/OutlinedButton";
 
 function LocationPicker() {
-  const [pickedLocation, setPickedLocation] = useState();
-
   const [locationPermissionInformation, requestPermission] =
     useForegroundPermissions();
 
@@ -44,30 +40,14 @@ function LocationPicker() {
     }
 
     const location = await getCurrentPositionAsync();
-    setPickedLocation({
-      lat: location.coords.latitude,
-      lng: location.coords.longitude,
-    });
+    console.log(location);
   }
 
   function pickOnMapHandler() {}
 
-  let locationPreview = <Text>No location picked yet.</Text>;
-
-  if (pickedLocation) {
-    locationPreview = (
-      <Image
-        style={styles.image}
-        source={{
-          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
-        }}
-      />
-    );
-  }
-
   return (
     <View>
-      <View style={styles.mapPreview}>{locationPreview}</View>
+      <View style={styles.mapPreview}></View>
       <View style={styles.actions}>
         <OutlinedButton icon="location" onPress={getLocationHandler}>
           Locate User
@@ -91,16 +71,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
-    overflow: "hidden",
   },
   actions: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    // borderRadius: 4,
   },
 });
