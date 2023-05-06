adfeed11d688bc3758e6f92af70430c50c14e45a
Aman07a <aman16.aa17@gmail.com>
2023-05-06 17:14:04 +0200

    Objects: Properties & Methods

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
-const name = "Amana";
-let age = 22;
-const hasHobbies = true;
-
-age = 23;
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies: " +
-    userHasHobby
-  );
+const person = {
+  name: "Aman",
+  age: 22,
+  greet() {
+    console.log("Hi, I am " + this.name);
+  },
 };
 
-// const add = (a, b) => a + b;
-// const addOne = (a) => a + 1;
-const addRandom = () => 1 + 2;
-
-// console.log(add(1, 2));
-// console.log(addOne(1));
-console.log(addRandom());
-
-console.log(summarizeUser(name, age, hasHobbies));
+person.greet();
